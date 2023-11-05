dd46364fde0d7b04f767226aca2e13b4504eb739
Zhongyi-Lu <lu.zhon@northeastern.edu>
2023-11-04 22:43:31 -0700

    use `.env`

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ import express from 'express';
 import admin from 'firebase-admin';
 import { createRequire } from 'module'; // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url); // construct the require method
-const serviceAccount = require('./firebase.json');
+// const serviceAccount = require('./firebase.json');
+require('dotenv').config();
 
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
+
+console.log(`serviceAccount ${JSON.stringify(serviceAccount)}`)
+
+// var serviceAccountJson = JSON.parse(myJsonString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
